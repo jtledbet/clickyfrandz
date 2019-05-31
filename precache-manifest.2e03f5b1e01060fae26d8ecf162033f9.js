d978f5536cff30f33f35f54b31dadb0c0296aa63
Jon <jon.ledbetter@gmail.com>
2019-05-31 17:08:11 -0400

    Updates

---
+++ precache-manifest.2e03f5b1e01060fae26d8ecf162033f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a328390f2a4d746e4f34cf990868cc",
+    "url": "/clickyfrandz/index.html"
+  },
+  {
+    "revision": "ec03f4253eecb73581dd",
+    "url": "/clickyfrandz/static/css/main.e4a623e0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickyfrandz/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ec03f4253eecb73581dd",
+    "url": "/clickyfrandz/static/js/main.492c2e7b.chunk.js"
+  },
+  {
+    "revision": "0bb55f4347b344cf492c",
+    "url": "/clickyfrandz/static/js/runtime~main.42da85d2.js"
+  },
+  {
+    "revision": "6b6da7ad35033f936490ef89f73376ff",
+    "url": "/clickyfrandz/static/media/cat.6b6da7ad.png"
+  },
+  {
+    "revision": "b136028125f54e8807a930f9e864b647",
+    "url": "/clickyfrandz/static/media/dog.b1360281.png"
+  }
+]);
